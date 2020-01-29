a4601f4ffc7dc218e414c341e9eeaa4a3d29797a
cwxn5 <cwxn5@mail.umkc.edu>
2020-01-29 01:06:15 -0600

    Update AppColumn.js
    Replace Semantic UI with Styled Components. Improved styling.

--- src/components/applications/AppColumn.js
+++ src/components/applications/AppColumn.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
-
-import { Header, Label } from "semantic-ui-react";
-import { Switch } from "antd";
+import { Switch, Badge } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
@@ -11,19 +9,52 @@ import AppCard from "./AppCard";
 import AppCreate from "./AppCreate";
 import getFilteredApplications from "../../selectors/application";
 
+const ColumnCountBadge = styled.div`
+  border-radius: 50%;
+  width: 27px;
+  height: 27px;
+  padding: 3px;
+  margin-left: 8px;
+  font-weight: bold;
+  background: black;
+  color: white;
+  text-align: center;
+`;
 const ColumnDiv = styled.div`
   min-width: 350px;
   width: 350px;
-  padding: 16px;
+  height: fit-content;
+  margin: 16px;
+  padding: 8px;
+  background-color: #f3f4f5;
+  border: 1px solid #d9d9d9;
+  border-radius: 4px;
+`;
+const ColumnHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: ${props => (props.cards ? "8px" : "0")};
+`;
+const ColumnHeaderTitle = styled.div`
+  display: flex;
+  align-items: center;
 `;
 const RightAlignItem = styled.div`
   float: right;
   text-align: right;
 `;
-
 const AppCardsDiv = styled.div`
+  border: ${props => (props.showCardAppsBorder ? "1px solid #d9d9d9" : "none")};
+  border-radius: 4px;
   overflow-y: auto;
-  max-height: 80%;
+  max-height: 75vh;
+`;
+const Title = styled.div`
+  font-weight: bold;
+  font-size: 20px;
+  color: black;
+  padding-left: 8px;
 `;
 
 class AppColumn extends React.Component {
@@ -61,16 +92,26 @@ class AppColumn extends React.Component {
     //list of all AppCards that match status of AppColumn
     return (
       <ColumnDiv>
-        <div className="columnHeader">
-          <Header as="h3" block textAlign="left">
-            {this.props.title}
-            <Label circular color="black">
-              {this.props.apps.length}
-            </Label>
-            {this.renderShowRejectedApps()}
-          </Header>
-        </div>
-        <AppCardsDiv>{this.renderAppCards()}</AppCardsDiv>
+        <ColumnHeader
+          cards={
+            (this.props.title !== "Rejected" || this.state.showCards) &&
+            this.props.apps.length > 0
+          }
+        >
+          <ColumnHeaderTitle>
+            <Title>{this.props.title}</Title>
+            <ColumnCountBadge>{this.props.apps.length}</ColumnCountBadge>
+          </ColumnHeaderTitle>
+          {this.renderShowRejectedApps()}
+        </ColumnHeader>
+        <AppCardsDiv
+          showCardAppsBorder={
+            (this.props.title !== "Rejected" || this.state.showCards) &&
+            this.props.apps.length > 0
+          }
+        >
+          {this.renderAppCards()}
+        </AppCardsDiv>
         {this.renderAppCreate()}
       </ColumnDiv>
     );
