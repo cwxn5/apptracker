8e86b6ee0474a81576fa35938cb05d28fb596474
Charlie Weidert <charlie@barn2door.com>
2021-03-07 14:07:00 -0600

    Remove redux dev tools from live environment

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,9 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import reducers from "../reducers";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.PRODUCTION
+  ? compose
+  : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
 export default () => {
   return createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
