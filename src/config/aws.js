a9ee190ca6eb218b7e8ee05ae59a30c20e6c674e
cwxn5 <43484268+cwxn5@users.noreply.github.com>
2021-11-17 20:33:36 -0800

    Update aws.js

--- src/config/aws.js
+++ src/config/aws.js
@@ -1,7 +1,7 @@
 const config = {
     region: 'us-east-2',
-    secretAccessKey: process.env.AWS_SECRET_KEY,
-    accessKeyId: process.env.AWS_ACCESS_KEY
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID
 }
 
 export default config;
